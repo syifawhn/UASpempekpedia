0563ea26d66d32eb28f2d6b2aa3d6d3b4f87251c
syifawhn <syifawahyuni@mhs.mdp.ac.id>
2023-06-22 18:53:03 +0700

    update

---
+++ app/src/main/java/uas/syifa/uaspempekpedia/Restoran.java
@@ -0,0 +1,77 @@
+package uas.syifa.uaspempekpedia;
+
+public class Restoran {
+
+    private String id;
+    private String nama_restoran;
+    private String lokasi;
+    private String telphone;
+    private String jam_operational;
+    private String rating;
+    private String tentang_restoran;
+    private String link_foto;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama_restoran() {
+        return nama_restoran;
+    }
+
+    public void setNama_restoran(String nama_restoran) {
+        this.nama_restoran = nama_restoran;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+
+    public String getJam_operational() {
+        return jam_operational;
+    }
+
+    public void setJam_operational(String jam_operational) {
+        this.jam_operational = jam_operational;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getTentang_restoran() {
+        return tentang_restoran;
+    }
+
+    public void setTentang_restoran(String tentang_restoran) {
+        this.tentang_restoran = tentang_restoran;
+    }
+
+    public String getLink_foto() {
+        return link_foto;
+    }
+
+    public void setLink_foto(String link_foto) {
+        this.link_foto = link_foto;
+    }
+}
